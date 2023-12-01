31f23a024f9ea68d66a16bff557af86d940f23f8
xxng1 <woongaa1@naver.com>
2023-12-01 10:47:59 +0900

    빌드 오류 의존성 추가

--- src/main/java/odo/server/presentation/OauthController.java
+++ src/main/java/odo/server/presentation/OauthController.java
@@ -18,8 +18,14 @@ import org.apache.tomcat.util.json.JSONParser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.configurationprocessor.json.JSONArray;
-import org.springframework.boot.configurationprocessor.json.JSONException;
-import org.springframework.boot.configurationprocessor.json.JSONObject;
+
+//import org.springframework.boot.configurationprocessor.json.JSONException;
+//import org.springframework.boot.configurationprocessor.json.JSONObject;
+//이거로 수정해야 빌드할때 오류 안남
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;

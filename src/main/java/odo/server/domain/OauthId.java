8af52d59849ff7ef4794f8bd67371240e6b41498
xxng1 <woongaaaaa1@gmail.com>
2024-03-28 17:02:32 +0900

    [Refactor] #1 주석 제거

--- src/main/java/odo/server/domain/OauthId.java
+++ src/main/java/odo/server/domain/OauthId.java
@@ -15,11 +15,9 @@ import static jakarta.persistence.EnumType.STRING;
 @AllArgsConstructor
 @NoArgsConstructor(access = PROTECTED)
 public class OauthId {
-    // 특정 인증 서버의 식별자 값을 의미하는 oauthServerId
     @Column(nullable = false, name = "oauth_server_id")
     private String oauthServerId;
 
-    // 이를 제공하는 서비스 타입
     @Enumerated(STRING)
     @Column(nullable = false, name = "oauth_server")
     private OauthServerType oauthServerType;
